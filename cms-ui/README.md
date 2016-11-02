daa79214b7a84c295633e3f91ac56df2eb94188c
Christophe Vaugeois <cavaugeois@gmail.com>
2016-11-01 22:55:21 -0400

    initial ui commit

---
+++ cms-ui/README.md
@@ -0,0 +1,12 @@
+# Koala UI
+UI for the Koala cms.
+
+##Prerequisites
+1. Install node
+
+##Run locally
+```bash
+npm install
+ln -s /path/to/koala/cms-rest/config.sample.js /path/to/koala/cms-rest/config.js
+npm start
+```
---
+++ cms-ui/lib/helpers/proxy.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const rp = require("request-promise");
+const config = require("../../config");
+
+function proxy(req, res, method) {
+    let options = {
+        method: method,
+        uri: config.api.host + req.path,
+        body: req.body,
+        json: true
+    };
+
+    return rp.post(options).then(function () {
+        res.sendStatus(200);
+    }).catch(function (error) {
+        res.sendStatus(403);
+    });
+}
+
+module.exports = proxy;
