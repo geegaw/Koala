a2895aafa3355ddd3e08a13306f930b281b8f3e6
Christophe Vaugeois <cavaugeois@gmail.com>
2016-11-08 20:11:15 -0500

    removing console

--- cms-ui/lib/code/browser/views/parts/userNavView.js
+++ cms-ui/lib/code/browser/views/parts/userNavView.js
@@ -5,10 +5,6 @@ const Marionette = require("backbone.marionette");
 const HeaderView = Marionette.View.extend({
     template: "parts/user-nav",
 
-    initialize: function() {
-        console.log(this.model);
-    },
-
     modelEvents: {
         "change:username": "render",
     },
