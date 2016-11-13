7ad3e680ff0fee330df1afba14152dcb8a27c7fd
geegaw <cavaugeois@gmail.com>
2016-11-13 09:23:26 -0500

    Reusable form layout (#22)

---
+++ cms-ui/lib/code/browser/views/pages/roles/roleView.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const Marionette = require("backbone.marionette");
+
+const PermissionsView = require("./permissionsView");
+const TextView = require("../../elements/form/textView");
+const TextareaView = require("../../elements/form/textareaView");
+
+const RoleView = Marionette.View.extend({
+    template: "pages/roles/roles-form",
+    className: "roles--form",
+
+    regions: {
+        name: ".roles--form--name",
+        description: ".roles--form--description",
+        permissions: ".roles--form--permissions",
+    },
+
+    onRender: function() {
+        this.getRegion("name").show(new TextView({
+            model: this.model,
+            field: "name",
+            placeholder: "name for the role",
+            label: "Name",
+            extraClass: "roles--form--name__input",
+        }));
+
+        this.getRegion("description").show(new TextareaView({
+            model: this.model,
+            field: "description",
+            placeholder: "short description for the role",
+            label: "Description",
+            extraClass: "roles--form--name__input",
+        }));
+
+        this.getRegion("permissions").show(new PermissionsView({
+            collection: this.model.get("permissions"),
+        }));
+
+    },
+
+});
+
+module.exports = RoleView;
--- cms-ui/lib/code/browser/views/pages/roles/rolesFormView.js
+++ cms-ui/lib/code/browser/views/pages/roles/rolesFormView.js
@@ -1,61 +1,14 @@
 "use strict";
 
-const Marionette = require("backbone.marionette");
+const FormLayoutView = require("../../elements/form/formLayoutView");
+const RoleView = require("./roleView");
 
-const CommandsView = require("../../elements/form/commandsView");
-const PermissionsView = require("./permissionsView");
-const TextView = require("../../elements/form/textView");
-const TextareaView = require("../../elements/form/textareaView");
-
-const RolesFormView = Marionette.View.extend({
-    template: "pages/roles/roles-form",
-    className: "koala-container roles--form",
-
-    initialize: function(){
-        if (this.model.id){
-            this.model
-                .fetch()
-                .done(this.render.bind(this));
-        }
-    },
-
-    templateContext: function() {
-        return {
-            action: this.id ? "Edit" : "Create",
-        };
-    },
-
-    regions: {
-        name: ".roles--form--name",
-        description: ".roles--form--description",
-        permissions: ".roles--form--permissions",
-        commands: ".roles--commands"
-    },
+const RolesFormView = FormLayoutView.extend({
 
     onRender: function() {
-        this.getRegion("name").show(new TextView({
+        FormLayoutView.prototype.onRender.apply(this);
+        this.getRegion("form").show(new RoleView({
             model: this.model,
-            field: "name",
-            placeholder: "name for the role",
-            label: "Name",
-            extraClass: "roles--form--name__input",
-        }));
-
-        this.getRegion("description").show(new TextareaView({
-            model: this.model,
-            field: "description",
-            placeholder: "short description for the role",
-            label: "Description",
-            extraClass: "roles--form--name__input",
-        }));
-
-        this.getRegion("permissions").show(new PermissionsView({
-            collection: this.model.get("permissions"),
-        }));
-
-        this.getRegion("commands").show(new CommandsView({
-            model: this.model,
-            delete: Boolean(this.model.id),
         }));
     },
 
