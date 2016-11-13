15b750c7396b692b08910571db2b2d07852d0301
Christophe Vaugeois <cavaugeois@gmail.com>
2016-11-12 20:20:07 -0500

    Basic input views

---
+++ cms-ui/lib/code/browser/views/elements/form/inputView.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Marionette = require("backbone.marionette");
+
+const InputView = Marionette.View.extend({
+    templateContext: function() {
+        return {
+            cid: this.cid,
+            label: this.getOption("label"),
+            field: this.getOption("field"),
+            extraClass: this.getOption("extraClass"),
+            placeholder: this.getOption("placeholder"),
+            value: this.model.get(this.getOption("field")),
+        };
+    },
+
+    setValue: function(evt) {
+        let value = evt.currentTarget.value;
+        this.model.set(this.getOption("field"), value);
+    },
+
+});
+
+module.exports = InputView;
