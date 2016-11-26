8fce36e1bde689f0c904443ca0734588afaa9dc0
Christophe Vaugeois <cavaugeois@gmail.com>
2016-11-26 17:16:00 -0500

    Descriptions for inputs

---
+++ cms-ui/lib/code/browser/views/elements/form/selectView.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const InputView = require("./inputView");
+
+const SelectView = InputView.extend({
+    template: "elements/form/select",
+
+    templateContext: function() {
+        let context = InputView.prototype.templateContext.apply(this);
+        let options = this.getOption("options") || [];
+
+        options.forEach(function(option) {
+            options.selected = context.value === option.value;
+        });
+        delete context.value;
+
+        return Object.assign({}, context, {options});
+    },
+
+    events: {
+        "change select": "setValue",
+    },
+
+});
+
+module.exports = SelectView;
