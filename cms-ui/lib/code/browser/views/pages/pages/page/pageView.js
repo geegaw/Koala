721c7d24a159fe4dd01a1a6407074fab93f5a46f
geegaw <cavaugeois@gmail.com>
2016-11-27 18:39:34 -0500

    Page template dropdown (#49)

--- cms-ui/lib/code/browser/views/pages/pages/page/pageView.js
+++ cms-ui/lib/code/browser/views/pages/pages/page/pageView.js
@@ -2,14 +2,22 @@
 
 const Marionette = require("backbone.marionette");
 
+const Templates = require("../../../../collections/Templates");
+const SelectView = require("../../../elements/form/selectView");
 const TextView = require("../../../elements/form/textView");
 
 const PageView = Marionette.View.extend({
     template: "pages/pages/pages-form",
     className: "pages--form",
 
+    initialize: function() {
+        this.templates = new Templates();
+        this.templates.fetch().then(this.render.bind(this));
+    },
+
     regions: {
         title: ".pages--form--title",
+        template: ".pages--form--template",
     },
 
     onRender: function() {
@@ -20,6 +28,25 @@ const PageView = Marionette.View.extend({
             label: "Title",
             extraClass: "pages--form--title__input",
         }));
+
+        this.getRegion("template").show(new SelectView({
+            model: this.model,
+            options: this.getTemplateOptions(),
+            field: "templateId",
+            label: "Template",
+            extraClass: "pages--form--template__input",
+        }));
+    },
+
+    getTemplateOptions: function() {
+        let formatted = [];
+        this.templates.each(function(template) {
+            formatted.push({
+                value: template.id,
+                label: template.get("name"),
+            });
+        });
+        return formatted;
     },
 
 });
